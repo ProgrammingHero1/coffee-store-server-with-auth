b337b48ab870048cfc27a2e251ac30d687d37284
Programming Hero <programming.hero1@gmail.com>
2023-05-04 17:26:20 -0500

    update a coffee api route created

--- index.js
+++ index.js
@@ -48,6 +48,28 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/coffee/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const options = { upsert: true };
+            const updatedCoffee = req.body;
+
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name, 
+                    quantity: updatedCoffee.quantity, 
+                    supplier: updatedCoffee.supplier, 
+                    taste: updatedCoffee.taste, 
+                    category: updatedCoffee.category, 
+                    details: updatedCoffee.details, 
+                    photo: updatedCoffee.photo
+                }
+            }
+
+            const result = await coffeeCollection.updateOne(filter, coffee, options);
+            res.send(result);
+        })
+
         app.delete('/coffee/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
